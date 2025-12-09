2f3ac5bd279f5162707f386842768138fb43f611
Zaamir Shaikh <shaikhzaamir04@gmail.com>
2025-12-09 14:59:56 +0530

    Refactor collections.ts to remove unused cache options and enforce no-store caching strategy

--- src/lib/data/collections.ts
+++ src/lib/data/collections.ts
@@ -5,16 +5,12 @@ import { HttpTypes } from "@medusajs/types"
 import { getCacheOptions } from "./cookies"
 
 export const retrieveCollection = async (id: string) => {
-  const next = {
-    ...(await getCacheOptions("collections")),
-  }
-
   return sdk.client
     .fetch<{ collection: HttpTypes.StoreCollection }>(
       `/store/collections/${id}`,
       {
-        next,
-        cache: "force-cache",
+        cache: "no-store",
+        next: { revalidate: 0 },
       }
     )
     .then(({ collection }) => collection)
@@ -23,10 +19,6 @@ export const retrieveCollection = async (id: string) => {
 export const listCollections = async (
   queryParams: Record<string, string> = {}
 ): Promise<{ collections: HttpTypes.StoreCollection[]; count: number }> => {
-  const next = {
-    ...(await getCacheOptions("collections")),
-  }
-
   queryParams.limit = queryParams.limit || "100"
   queryParams.offset = queryParams.offset || "0"
 
@@ -35,8 +27,8 @@ export const listCollections = async (
       "/store/collections",
       {
         query: queryParams,
-        next,
-        cache: "force-cache",
+        cache: "no-store",
+        next: { revalidate: 0 },
       }
     )
     .then(({ collections }) => ({ collections, count: collections.length }))
@@ -45,15 +37,11 @@ export const listCollections = async (
 export const getCollectionByHandle = async (
   handle: string
 ): Promise<HttpTypes.StoreCollection> => {
-  const next = {
-    ...(await getCacheOptions("collections")),
-  }
-
   return sdk.client
     .fetch<HttpTypes.StoreCollectionListResponse>(`/store/collections`, {
       query: { handle, fields: "*products" },
-      next,
-      cache: "force-cache",
+      cache: "no-store",
+      next: { revalidate: 0 },
     })
     .then(({ collections }) => collections[0])
 }
